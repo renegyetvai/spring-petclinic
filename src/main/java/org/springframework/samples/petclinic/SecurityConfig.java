467253277b490ca20ffd9a4ece0de3f23d2341a6
René Gyetvai <rene.gyetvai@googlemail.com>
2024-02-06 23:25:06 +0100

    testing other config for pipeline

--- src/main/java/org/springframework/samples/petclinic/SecurityConfig.java
+++ src/main/java/org/springframework/samples/petclinic/SecurityConfig.java
@@ -3,17 +3,17 @@ package org.springframework.samples.petclinic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.requiresChannel(channel -> channel.anyRequest().requiresSecure());
-		return http.build();
+		return http
+			.requiresChannel(channel -> channel.anyRequest().requiresSecure())
+			.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
+			.build();
 	}
 
 }
--- src/test/java/org/springframework/samples/petclinic/PetClinicIntegrationTests.java
+++ src/test/java/org/springframework/samples/petclinic/PetClinicIntegrationTests.java
@@ -51,7 +51,7 @@ public class PetClinicIntegrationTests {
 
 	@Test
 	void testOwnerDetails() {
-		RestTemplate template = builder.rootUri("https://localhost:" + port).build();
+		RestTemplate template = builder.rootUri("http://localhost:" + port).build();
 		ResponseEntity<String> result = template.exchange(RequestEntity.get("/owners/1").build(), String.class);
 		assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
